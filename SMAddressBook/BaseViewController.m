d3e1fe904db6f43d1502bf7ca4cebc403b8c4e98
bleugirl <sochae78@gmail.com>
2013-08-27 22:37:49 +0900

    네비게이션 오른쪽 버튼 툴바 추가

--- SMAddressBook/BaseViewController.m
+++ SMAddressBook/BaseViewController.m
@@ -63,15 +63,20 @@
 /// 네비게이션바 버튼 설정
 - (void)setupMenuBarButtonItems
 {
-    if(self.menuContainerViewController.menuState == MFSideMenuStateClosed && ![[self.navigationController.viewControllers objectAtIndex:0] isEqual:self])
-        {
-        // 최상위 화면이 아니면 [이전]버튼 표시
-        self.navigationItem.leftBarButtonItem = [self backBarButtonItem];
-        
-        } else {
-            // 최상위 화면이면 [메뉴]버튼 표시
-            self.navigationItem.leftBarButtonItem = [self leftMenuBarButtonItem];
-        }
+    // 디자인 구조상 왼쪽은 무조건 메뉴 버튼을 고정.
+//    if(self.menuContainerViewController.menuState == MFSideMenuStateClosed && ![[self.navigationController.viewControllers objectAtIndex:0] isEqual:self])
+//        {
+//        // 최상위 화면이 아니면 [이전]버튼 표시
+//        self.navigationItem.leftBarButtonItem = [self backBarButtonItem];
+//        
+//        } else {
+//            // 최상위 화면이면 [메뉴]버튼 표시
+//            self.navigationItem.leftBarButtonItem = [self leftMenuBarButtonItem];
+//        }
+    
+
+    self.navigationItem.leftBarButtonItem = [self leftMenuBarButtonItem];
+    self.navigationItem.rightBarButtonItem = [self rightMenuBarButtonItem];
 }
 
 /// 네비게이션 왼쪽 [메뉴] 버튼
@@ -113,7 +118,7 @@
 /// 네비게이션 오른쪽 버튼
 - (UIBarButtonItem *)rightMenuBarButtonItem
 {
-    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 100.0f, 44.01f)]; // 44.01 shifts it up 1px for some reason
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 120.0f, 44.01f)]; // 44.01 shifts it up 1px for some reason
     [toolbar setBarStyle: UIBarStyleBlackOpaque];
     
     toolbar.clearsContextBeforeDrawing = NO;
@@ -129,11 +134,12 @@
     
     // create an array for the buttons
     NSMutableArray* buttons = [[NSMutableArray alloc] initWithCapacity:3];
+    CGFloat size = 30.0f;
     
     /* 닫기 버튼 */
     UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    closeButton.frame = CGRectMake(0.0f, 0.0f, 25.0f, 25.0f);
-    [closeButton setImage:[UIImage imageNamed:@"web_pre_off"] forState:UIControlStateNormal];
+    closeButton.frame = CGRectMake(0.0f, 0.0f, size, size);
+    [closeButton setImage:[UIImage imageNamed:@"btn_back"] forState:UIControlStateNormal];
     [closeButton addTarget:self action:@selector(onCloseButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
     
     barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:closeButton];
@@ -143,8 +149,8 @@
     
     /* 홈 버튼 */
     UIButton *homeButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    homeButton.frame = CGRectMake(0.0f, 0.0f, 25.0f, 25.0f);
-    [homeButton setImage:[UIImage imageNamed:@"profile_iconbtn_9"] forState:UIControlStateNormal];
+    homeButton.frame = CGRectMake(0.0f, 0.0f, size, size);
+    [homeButton setImage:[UIImage imageNamed:@"btn_home"] forState:UIControlStateNormal];
     [homeButton addTarget:self action:@selector(onHomeButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
     
     barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:homeButton];
@@ -154,8 +160,8 @@
     
     /* 검색 버튼 */
     UIButton *searchButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    searchButton.frame = CGRectMake(0.0f, 0.0f, 25.0f, 25.0f);
-    [searchButton setImage:[UIImage imageNamed:@"common_input_search_icon"] forState:UIControlStateNormal];
+    searchButton.frame = CGRectMake(0.0f, 0.0f, size, size);
+    [searchButton setImage:[UIImage imageNamed:@"btn_filter"] forState:UIControlStateNormal];
     [searchButton addTarget:self action:@selector(onSearchButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
     
     barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:searchButton];
