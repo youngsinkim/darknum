34d452170521f6f839ea510aa5f69818b60bfea9
bleugirl <sochae78@gmail.com>
2013-09-08 02:32:29 +0900

    즐겨찾기 목록 DB 연동하여 섹션별 구성

--- SMAddressBook/FavoriteSettingViewController.m
+++ SMAddressBook/FavoriteSettingViewController.m
@@ -12,7 +12,8 @@
 @interface FavoriteSettingViewController ()
 
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (strong, nonatomic) UITableView *classTableView;
+@property (strong, nonatomic) UITableView *fvSettTableView;
+@property (strong, nonatomic) NSMutableArray *courses;
 @property (strong, nonatomic) NSMutableArray *courseClasses;
 
 @end
@@ -25,6 +26,9 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.navigationItem.title = @"즐겨찾기 설정";
+        
+        _courses = [[NSMutableArray alloc] initWithCapacity:4];
         _courseClasses = [[NSMutableArray alloc] initWithCapacity:4];
     }
     return self;
@@ -47,10 +51,51 @@
     // 즐겨찾기 화면 구성
     [self setupFavoriteSettingUI];
     
-    // 과정별 기수 목록 DB에서 가져오기
-    _courseClasses = [self loadDBCourseClasses];
+    // DB 과정 목록
+//    _courses = [@[@{@"course":@"교수진/교직원"}] mutableCopy];
+//    NSArray *items = @[@{@"course":@"교수진/교직원"}, @{@"courseclass":@""}];
+//    [_courses addObject:items];
+//    [_courses addObjectsFromArray:[self loadDBCourses]];
+    NSArray *tmpCourses = [self loadDBCourses];
+    NSLog(@"과정 수: %@", tmpCourses);
     
-    [self.classTableView reloadData];
+    
+    // 과정(섹션) 기수 목록 (교수/교직원 먼저 가져오고 나머지 과정별로 가져온다)
+//    [_courseClasses addObject:[self loadDBStaticCourseClasses]];
+
+    NSArray *tmpList = [self loadDBStaticCourseClasses];
+    NSLog(@"고정 기수 목록 : %d", [tmpList count]);
+
+    NSMutableArray *tmpClasses = [[NSMutableArray alloc] init];
+    for (NSDictionary *dict in tmpCourses)
+    {
+        NSLog(@"Before 과정 정보 : %@", dict);
+        
+        // 각 과정 그룹별 기수 목록 가져와서 트리 구성.
+        NSArray *filterd = [self loadDBCourseClasses:dict[@"course"]];
+        
+        if ([filterd count] > 0) {
+            //            NSDictionary *subItems = [NSDictionary dictionaryWithObject:filterd forKey:@"courseclass"];// @{@"courseclass":filterd};
+            //            [courseInfo addEntriesFromDictionary:subItems];//@{@"subItem":filterd}];
+            //            NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithDictionary:courseInfo];
+            //            [dict setObject:filterd forKey:@"courseclass"];
+            
+            [tmpClasses addObject:filterd];
+        }
+        
+        NSLog(@"After 과정 정보 : %@", _courseClasses);
+    }
+    
+    _courses = [@[@{@"course":@"교수진/교직원"}] mutableCopy];
+    [_courses addObjectsFromArray:tmpCourses];
+    
+    [_courseClasses setArray:@[tmpList]];
+    [_courseClasses addObjectsFromArray:[tmpClasses mutableCopy]];
+
+
+    NSLog(@"전체 기수 목록 : %d - %d", [_courses count], [_courseClasses count]);
+    
+    [_fvSettTableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -62,30 +107,134 @@
 - (void)setupFavoriteSettingUI
 {
     // 과정 기수 목록 구성
-    CGRect rect = [[UIScreen mainScreen] applicationFrame];
+    CGRect rect = self.view.bounds;
     
-    _classTableView = [[UITableView alloc] initWithFrame:self.view.frame];
-    _classTableView.dataSource = self;
-    _classTableView.delegate = self;
+    _fvSettTableView = [[UITableView alloc] initWithFrame:rect style:UITableViewStyleGrouped];
+    _fvSettTableView.dataSource = self;
+    _fvSettTableView.delegate = self;
     
-    [self.view addSubview:_classTableView];
+    [self.view addSubview:_fvSettTableView];
 
 }
 
 
 #pragma mark - DB methods
-/// 과정별 기수 목록 트리 구조로 구성
 
-- (NSMutableArray *)loadDBCourseClasses
+/// 과정 DB 목록
+- (NSArray *)loadDBCourses
 {
-    // 0(EMBA), 1(GMBA), 2(SMBA)
+    if (self.managedObjectContext == nil) {
+        return nil;
+    }
+    
+    NSError *error = nil;
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Course" inManagedObjectContext:self.managedObjectContext];
+    [fetchRequest setEntity:entity];
+    
+    NSAttributeDescription *course = [entity.attributesByName objectForKey:@"course"];
+//    NSAttributeDescription *courseclass = [entity.attributesByName objectForKey:@"courseclass"];
+    [fetchRequest setPropertiesToFetch:[NSArray arrayWithObjects:course, nil]];
+    [fetchRequest setPropertiesToGroupBy:[NSArray arrayWithObject:course]];
+    [fetchRequest setResultType:NSDictionaryResultType];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(courseclass != '')"];
+    [fetchRequest setPredicate:predicate];
+    
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"courseclass" ascending:YES];
+    [fetchRequest setSortDescriptors:[NSArray arrayWithObjects:sortDescriptor, nil]];
+    
+    NSArray *fetchedObjects = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
+    NSLog(@"DB data count : %d", [fetchedObjects count]);
+    
+    for (NSDictionary *info in fetchedObjects) {
+        NSLog(@"fetchedObject : %@", info);
+    }
+    
+    return fetchedObjects;
+}
+
+/// 과정별 기수 목록 (by type)
+- (NSArray *)loadDBStaticCourseClasses
+{
+    if (self.managedObjectContext == nil) {
+        return nil;
+    }
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Course" inManagedObjectContext:self.managedObjectContext];
+    [fetchRequest setEntity:entity];
+    
+//    NSAttributeDescription *type = [entity.attributesByName objectForKey:@"course"];
+//    [fetchRequest setPropertiesToFetch:[NSArray arrayWithObjects:type, nil]];
+//    [fetchRequest setResultType:NSDictionaryResultType];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(courseclass == '')"];
+    [fetchRequest setPredicate:predicate];
+    
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"courseclass" ascending:YES];
+    [fetchRequest setSortDescriptors:[NSArray arrayWithObjects:sortDescriptor, nil]];
+    
+    NSError *error = nil;
+    NSArray *fetchedObjects = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
+    NSLog(@"DB data count : %d", [fetchedObjects count]);
+    
+    for (Course *class in fetchedObjects) {
+        NSLog(@"과정 정보 : %@", class.title);
+    }
+    
+    return fetchedObjects;
+}
+
+
+/// 과정별 기수 목록 (by course)
+- (NSArray *)loadDBCourseClasses:(NSString *)courseName//:(NSInteger)tabIndex
+{
+    if (self.managedObjectContext == nil) {
+        return nil;
+    }
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Course" inManagedObjectContext:self.managedObjectContext];
+    [fetchRequest setEntity:entity];
+    
+//    NSAttributeDescription *type = [entity.attributesByName objectForKey:@"course"];
+//    [fetchRequest setPropertiesToFetch:[NSArray arrayWithObjects:type, nil]];
+//    [fetchRequest setResultType:NSDictionaryResultType];
+    
+    // where
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"(course == %@)", courseName];
+    [fetchRequest setPredicate:predicate];
+    
+    // order by (ZCOURSECLASS)
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"courseclass" ascending:YES];
+    [fetchRequest setSortDescriptors:[NSArray arrayWithObjects:sortDescriptor, nil]];
+    
+    NSError *error = nil;
+    NSArray *fetchedObjects = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
+    NSLog(@"DB data count : %d", [fetchedObjects count]);
+    
+    for (Course *class in fetchedObjects) {
+        NSLog(@"title : %@", class.title);
+    }
+    
+    if (fetchedObjects && [fetchedObjects count] > 0)
+    {
+        return fetchedObjects;
+    }
+    return nil;
+}
+
+
+/// 과정별 기수 목록 트리 구조로 구성
+- (NSMutableArray *)loadDBCourseClasses1
+{
     // 교수/교직원 - 교수, 교직원
     // EMBA     - 1기, 2기, 3기, ...
     // GMBA     - 1기, 2기, 3기, ...
     // SMBA     - 1기, 2기, 3기, ...
 
-    NSError *error = nil;
-    
     if (self.managedObjectContext == nil) {
         return nil;
     }
@@ -134,6 +283,7 @@
     //    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"courseclass" ascending:YES];
     //    [fetchRequest setSortDescriptors:[NSArray arrayWithObjects:sortDescriptor, nil]];
     
+    NSError *error = nil;
     NSArray *fetchedObjects = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
     NSLog(@"DB data count : %d", [fetchedObjects count]);
     
@@ -178,55 +328,67 @@
 #pragma mark - UITableView DataSources
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return ([_courseClasses count] > 0)? [_courseClasses count] : 1;   // 교수,교직 / EMBA / GMBA / SMBA;
+    return ([_courses count] > 0)? [_courses count] : 1;   // 교수,교직 / EMBA / GMBA / SMBA;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSString *sectionName = _courses[section][@"course"];
+    NSLog(@"섹션 이름 : %@", sectionName);
+    
+    return sectionName;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-//    if ([_courseClasses count] > 0)
-//    {
-//        return _courseClasses[section].count;
-//    }
-//    return 1;
-    if (section == 0) {
-        return 2;
+    NSArray *courseClasses = _courseClasses[section];
+    NSLog(@"section : %@", courseClasses);
+    
+    if (courseClasses) {
+        return ([courseClasses count] > 0)? [courseClasses count] : 1;
     }
-    return 3;
+    return 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *identifier = @"CourseClassCell";
-    UITableViewCell *cell = [self.classTableView dequeueReusableCellWithIdentifier:identifier];
+    NSArray *list = _courseClasses[indexPath.section];
+    if (list == nil) {
+        return nil;
+    }
+    
+    if ([list count] == 0)
+    {
+        static NSString *identifier = @"NoFvSettingCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+        
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        }
+        
+        return cell;
+    }
+    
+    static NSString *identifier = @"FvSettingCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-        
-        UIView *myBackgroundView = [[UIView alloc] init];
-        myBackgroundView.frame = CGRectMake(0.0f, 0.0f, 20.0f, 20.0f);// cell.frame;
-        myBackgroundView.backgroundColor = [UIColor greenColor];
-        
-        [cell.backgroundView addSubview:myBackgroundView];
-//        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+//        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
-
-    NSLog(@"%d / %d", indexPath.row / [_courseClasses count]);
-    if ([_courseClasses count] > 0)
+    
+    if ([list count] > 0)
     {
-//        Course *course = _courseClasses[indexPath.section][indexPath.row];
-//        NSLog(@"셀 정보 : %@", course.title);
         // 주소록 셀 정보
-//        NSDictionary *cellInfo = [_contacts objectAtIndex:indexPath.row];
-//        NSLog(@"즐겨찾기 셀(%d) : %@", indexPath.row, [cellInfo description]);
-
-//        cell.textLabel.text = cellInfo[@"emba 1기"];
-//        cell.cellInfo = cellInfo;
+        Course *course = [list objectAtIndex:indexPath.row];
+        NSLog(@"즐겨찾기 셀(%d) : %@", indexPath.row, course.title);
         
-        cell.textLabel.text = [NSString stringWithFormat:@"%d기", arc4random()%10];
+        cell.textLabel.text = course.title;
+        //        cell.cellInfo = cellInfo;
     }
     
     return cell;
-
 }
 
 @end
